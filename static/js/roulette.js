98ae6d943effeb598efecfad7dd6fe1936f46988
Logan Fuller <logankfuller@gmail.com>
2019-06-10 23:29:21 -0700

    Added dummy images for when the game does not have a logo (often occurs when the game is taken off of Steam)

--- static/js/roulette.js
+++ static/js/roulette.js
@@ -18,13 +18,22 @@ $(function() {
                 username: mUsername
             },
             success: [ function (result) {
-                let html =
-                "<h1>" + result.name + "</h1>" +
-                "<a href='steam://run/" + result['app_id'] + "'>" +
-                    "<img alt='game logo' src='https://media.steampowered.com/steamcommunity/public/images/apps/" + result['app_id'] +
-                    "/" + result['img_logo_url'] + ".jpg'</img></a>";
-                $('#results').html(html);
-                console.log(result);
+                if(result['img_logo_url'] === "") {
+                    let html =
+                    "<h1>" + result.name + "</h1>" +
+                    "<a href='steam://run/" + result['app_id'] + "'>" +
+                        "<img alt='game logo' src='https://dummyimage.com/184x69/6D7680/fff.jpg&text=${result.name}'</img></a>";
+                    $('#results').html(html);
+                    console.log(result);
+                } else {
+                    let html =
+                    "<h1>" + result.name + "</h1>" +
+                    "<a href='steam://run/" + result['app_id'] + "'>" +
+                        "<img alt='game logo' src='https://media.steampowered.com/steamcommunity/public/images/apps/" + result['app_id'] +
+                        "/" + result['img_logo_url'] + ".jpg'</img></a>";
+                    $('#results').html(html);
+                    console.log(result);
+                }
             }]
         });
     });
